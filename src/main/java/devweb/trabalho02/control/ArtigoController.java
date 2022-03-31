e0e702851217bfc3f203acc3e5b7f2d5f9bbbf49
hironha <rafaelhirokato@gmail.com>
2022-03-30 21:26:40 -0300

    :sparkles: add criação de artigo

--- src/main/java/devweb/trabalho02/control/ArtigoController.java
+++ src/main/java/devweb/trabalho02/control/ArtigoController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -29,10 +30,11 @@ public class ArtigoController {
 
   /// POST /api/artigos -> criar artigo
   @PostMapping("/artigos")
-  public ResponseEntity<Artigo> createArtigo(@RequestBody Artigo art) {
+  public ResponseEntity<Artigo> createArtigo(@ModelAttribute("artigo") Artigo art) {
     try {
-      // art.getPublicado()
-      Artigo _art = artRep.save(new Artigo(art.getTitulo(), art.getResumo(), true));
+
+      Artigo _art = artRep.save(new Artigo(art.getTitulo(), art.getResumo(), art.getPublicado()));
+
       return new ResponseEntity<>(_art, HttpStatus.CREATED);
     } catch (Exception e) {
       return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
--- src/main/java/devweb/trabalho02/control/EditoraController.java
+++ src/main/java/devweb/trabalho02/control/EditoraController.java
@@ -1,20 +1,42 @@
 package devweb.trabalho02.control;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import devweb.trabalho02.model.Artigo;
+import devweb.trabalho02.repository.ArtigoRepository;
 
 @Controller
 class EditoraController {
+  @Autowired
+  ArtigoRepository artRep;
 
   /*
    * Obs.: retirando os comentários do código, ao fazer o logout
    * você será redirecionado para a tela de login novamente.
    */
   @GetMapping("/editora")
-  String index(Authentication a) {
+  public String showForm(Authentication a, Model model) {
+    Artigo artigo = new Artigo();
+    model.addAttribute("artigo", artigo);
     if (a != null)
-      return "editora.html";
+      return "editora";
+
+    return "redirect:oauth2/authorization/cognito";
+  }
+
+  @PostMapping("/editora")
+  public String registerForm(Authentication a, @ModelAttribute("artigo") Artigo artigo) {
+    if (a != null) {
+      artRep.save(artigo);
+      return "editora";
+    }
+
     return "redirect:oauth2/authorization/cognito";
   }
 }
